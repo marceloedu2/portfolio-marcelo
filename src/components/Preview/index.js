77b3baad8888ca8ab21c159588c41e5135f504ea
marceloedu2 <marcelo.edu2@gmail.com>
2023-05-22 13:13:36 -0300

    text correction

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -5,12 +5,10 @@ const Preview = () => {
   return (
     <Container>
       <Image src={require("../../assets/avatar.png")} alt="Avatar" />
-      <Title>
-        Olá, Sou Marcelo Araujo, desenvolvedor front-end.
-      </Title>
+      <Title>Olá, Sou Marcelo Araujo, desenvolvedor front-end.</Title>
       <SubTitle>
-        Gosto de ajudar empresas iniciantes, pwequenas empresas e agências a obter sites
-        de alta qualidade e uma experiência excepcional do usuário.
+        Gosto de ajudar empresas iniciantes, pequenas empresas e agências a
+        obter sites de alta qualidade e uma experiência excepcional do usuário.
       </SubTitle>
     </Container>
   )
